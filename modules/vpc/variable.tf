058125c076d476540c3d73ca5b509d196ae02182
shubham <shubhamtippe9@gmail.com>
2026-03-18 13:39:21 +0530

    modules added

--- modules/vpc/variable.tf
+++ modules/vpc/variable.tf
@@ -2,7 +2,7 @@ variable "vpc_cidr" {
     default = "192.21.0.0/16"
 }
 variable "subnet_cidr" {
-    default = "172.21.1.0/18"  
+    default = "192.21.0.0/20"  
 }
 variable "az" {
     default = "us-east-1a"
